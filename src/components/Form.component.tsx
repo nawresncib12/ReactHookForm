2ec82e0109d72c35af12501a189f192ff55b5dd4
nawresncib12 <nawresncib@insat.u-carthage.tn>
2023-01-22 00:15:13 +0100

    resetting

--- src/components/Form.component.tsx
+++ src/components/Form.component.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useForm } from "react-hook-form";
 import Friends from "./Friends.component";
 interface User {
@@ -18,15 +19,20 @@ const Form = () => {
     level: "",
   };
   const {
+    getValues,
+    reset,
     setValue,
     watch,
     register,
     handleSubmit,
+    trigger,
+    formState,
     formState: { errors, isValid },
   } = useForm({
     mode: "onBlur",
     defaultValues: defaults,
   });
+  const firstName = watch("firstName");
   const invalidMsg: string = "Field is invalid";
   const inputClasses =
     "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 ";
@@ -38,176 +44,249 @@ const Form = () => {
     setValue("courses", ["P", "S"], { shouldValidate: true });
     setValue("level", "1", { shouldValidate: true });
   };
+  const resetWal = () => {
+    reset({ ...getValues(), firstName: "wala" });
+  };
+  const resetForm = () => {
+    reset(defaults);
+  };
+  const resetAllButName = () => {
+    reset({ ...defaults, firstName: firstName });
+  };
+  const validateName = () => {
+    //trigger() whole form
+    trigger("firstName", { shouldFocus: true });
+  };
+  const fakeErrors = () => {};
+  React.useEffect(() => {
+    //we reset only here cause if we reset in the onsubmit function
+    //sometimes it reset before submit cause it's async
+    if (formState.isSubmitSuccessful) {
+      reset();
+    }
+  }, [formState, reset]);
   return (
     <form
       onSubmit={handleSubmit((data) => {
         console.log(data);
       })}
-      className="flex flex-col gap-5"
+      className="flex gap-20"
     >
-      {/* watch watches inout and returns its value */}
-      <p>Hi {watch("firstName")}</p>
-      <input
-        className={inputClasses + (errors.firstName && "border-red-500")}
-        placeholder="First name"
-        {...register("firstName", {
-          required: {
-            value: true,
-            message: invalidMsg,
-          },
-        })}
-      />
-
-      <input
-        {...register("lastName", {
-          pattern: {
-            value: /[A-Za-z]{3}/,
-            message: invalidMsg,
-          },
-        })}
-        className={inputClasses + (errors.lastName && "border-red-500")}
-        placeholder="Last name"
-      />
-      <input
-        {...register("age", {
-          valueAsNumber: true,
-          validate: (value) => value > 12 || invalidMsg,
-        })}
-        type="number"
-        className={inputClasses + (errors.age && "border-red-500")}
-        placeholder="Age"
-      />
-
-      <select
-        {...register("gender", {
-          required: {
-            value: true,
-            message: invalidMsg,
-          },
-        })}
-        className={inputClasses}
-      >
-        <option value="">Gender...</option>
-        <option value="F">Female</option>
-        <option value="m">Male</option>
-      </select>
-
-      <div>
-        <div className="flex items-center">
+      <div className="flex flex-col gap-5">
+        {/* watch watches inout and returns its value */}
+        <p>{firstName}</p>
+        <div>
           <input
-            {...register("courses", { required: true })}
-            id="painting"
-            type="checkbox"
-            value="P"
-            className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500"
+            className={inputClasses + (errors.firstName && "border-red-500")}
+            placeholder="First name"
+            {...register("firstName", {
+              required: {
+                value: true,
+                message: invalidMsg,
+              },
+            })}
           />
-          <label
-            htmlFor="painting"
-            className="ml-2 text-sm font-medium text-pink-800"
-          >
-            Painting
-          </label>
+          <p className="text-base text-pink-800">
+            {errors?.firstName?.message}
+          </p>
         </div>
-        <div className="flex items-center">
+        <div>
           <input
-            {...register("courses")}
-            id="singing"
-            type="checkbox"
-            value="S"
-            className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500"
+            {...register("lastName", {
+              pattern: {
+                value: /[A-Za-z]{3}/,
+                message: invalidMsg,
+              },
+            })}
+            className={inputClasses + (errors.lastName && "border-red-500")}
+            placeholder="Last name"
           />
-          <label
-            htmlFor="singing"
-            className="ml-2 text-sm font-medium text-pink-800"
-          >
-            Singing
-          </label>
+          <p className="text-base text-pink-800">{errors?.lastName?.message}</p>
         </div>
-        <div className="flex items-center">
+        <div>
           <input
-            {...register("courses")}
-            id="dancing"
-            type="checkbox"
-            value="D"
-            className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500"
+            {...register("age", {
+              valueAsNumber: true,
+              validate: (value) => value > 12 || invalidMsg,
+            })}
+            type="number"
+            className={inputClasses + (errors.age && "border-red-500")}
+            placeholder="Age"
           />
-          <label
-            htmlFor="dancing"
-            className="ml-2 text-sm font-medium text-pink-800"
-          >
-            Dancing
-          </label>
+          <p className="text-base text-pink-800">{errors?.age?.message}</p>
         </div>
+        <div>
+          <select
+            {...register("gender", {
+              required: {
+                value: true,
+                message: invalidMsg,
+              },
+            })}
+            className={inputClasses}
+          >
+            <option value="">Gender...</option>
+            <option value="F">Female</option>
+            <option value="m">Male</option>
+          </select>
+          <p className="text-base text-pink-800">{errors?.gender?.message}</p>
+        </div>
+        <div>
+          <div className="flex items-center">
+            <input
+              {...register("courses", {
+                required: { value: true, message: invalidMsg },
+              })}
+              id="painting"
+              type="checkbox"
+              value="P"
+              className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500"
+            />
+            <label
+              htmlFor="painting"
+              className="ml-2 text-sm font-medium text-pink-800"
+            >
+              Painting
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              {...register("courses")}
+              id="singing"
+              type="checkbox"
+              value="S"
+              className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500"
+            />
+            <label
+              htmlFor="singing"
+              className="ml-2 text-sm font-medium text-pink-800"
+            >
+              Singing
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              {...register("courses")}
+              id="dancing"
+              type="checkbox"
+              value="D"
+              className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500"
+            />
+            <label
+              htmlFor="dancing"
+              className="ml-2 text-sm font-medium text-pink-800"
+            >
+              Dancing
+            </label>
+          </div>
+          <p className="text-base text-pink-800">{errors?.courses?.message}</p>
+        </div>
+        <div>
+          <div className="flex items-center">
+            <input
+              {...register("level", {
+                required: { value: true, message: invalidMsg },
+              })}
+              id="beginner"
+              type="radio"
+              value={1}
+              className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500"
+            />
+            <label
+              htmlFor="beginner"
+              className="ml-2 text-sm font-medium text-pink-800"
+            >
+              Beginner
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              {...register("level", {
+                required: { value: true, message: invalidMsg },
+              })}
+              id="intermediate"
+              type="radio"
+              value={2}
+              className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500"
+            />
+            <label
+              htmlFor="intermediate"
+              className="ml-2 text-sm font-medium text-pink-800"
+            >
+              Intermediate
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              {...register("level", {
+                required: { value: true, message: invalidMsg },
+              })}
+              id="expert"
+              type="radio"
+              value={3}
+              className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500"
+            />
+            <label
+              htmlFor="expert"
+              className="ml-2 text-sm font-medium text-pink-800"
+            >
+              Expert
+            </label>
+          </div>
+          <p className="text-base text-pink-800">{errors?.level?.message}</p>
+        </div>
+        <button
+          disabled={!isValid}
+          className="disabled:bg-gray-400 disabled:text-gray-500 disabled:border-gray-500 bg-transparent hover:bg-pink-500 text-pink-700 font-semibold hover:text-white py-2 px-4 border border-pink-500 hover:border-transparent rounded"
+          type="submit"
+        >
+          submit
+        </button>
       </div>
-
-      <div>
-        <div className="flex items-center">
-          <input
-            {...register("level", {
-              required: { value: true, message: invalidMsg },
-            })}
-            id="beginner"
-            type="radio"
-            value={1}
-            className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500"
-          />
-          <label
-            htmlFor="beginner"
-            className="ml-2 text-sm font-medium text-pink-800"
-          >
-            Beginner
-          </label>
-        </div>
-        <div className="flex items-center">
-          <input
-            {...register("level", {
-              required: { value: true, message: invalidMsg },
-            })}
-            id="intermediate"
-            type="radio"
-            value={2}
-            className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500"
-          />
-          <label
-            htmlFor="intermediate"
-            className="ml-2 text-sm font-medium text-pink-800"
-          >
-            Intermediate
-          </label>
-        </div>
-        <div className="flex items-center">
-          <input
-            {...register("level", {
-              required: { value: true, message: invalidMsg },
-            })}
-            id="expert"
-            type="radio"
-            value={3}
-            className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500"
-          />
-          <label
-            htmlFor="expert"
-            className="ml-2 text-sm font-medium text-pink-800"
-          >
-            Expert
-          </label>
-        </div>
+      <div className="flex flex-col gap-10 justify-center">
+        <button
+          className="disabled:bg-gray-400 disabled:text-gray-500 disabled:border-gray-500 bg-transparent hover:bg-pink-500 text-pink-700 font-semibold hover:text-white py-2 px-4 border border-pink-500 hover:border-transparent rounded"
+          type="button"
+          onClick={randomize}
+        >
+          Random
+        </button>
+        <button
+          className="disabled:bg-gray-400 disabled:text-gray-500 disabled:border-gray-500 bg-transparent hover:bg-pink-500 text-pink-700 font-semibold hover:text-white py-2 px-4 border border-pink-500 hover:border-transparent rounded"
+          type="button"
+          onClick={validateName}
+        >
+          Trigger validation
+        </button>
+        <button
+          className="disabled:bg-gray-400 disabled:text-gray-500 disabled:border-gray-500 bg-transparent hover:bg-pink-500 text-pink-700 font-semibold hover:text-white py-2 px-4 border border-pink-500 hover:border-transparent rounded"
+          type="button"
+          onClick={fakeErrors}
+        >
+          Fake errors
+        </button>
+        <button
+          className="disabled:bg-gray-400 disabled:text-gray-500 disabled:border-gray-500 bg-transparent hover:bg-pink-500 text-pink-700 font-semibold hover:text-white py-2 px-4 border border-pink-500 hover:border-transparent rounded"
+          type="button"
+          onClick={resetWal}
+        >
+          Reset name to wala
+        </button>
+        <button
+          className="disabled:bg-gray-400 disabled:text-gray-500 disabled:border-gray-500 bg-transparent hover:bg-pink-500 text-pink-700 font-semibold hover:text-white py-2 px-4 border border-pink-500 hover:border-transparent rounded"
+          type="button"
+          onClick={resetForm}
+        >
+          Reset all
+        </button>
+        <button
+          className="disabled:bg-gray-400 disabled:text-gray-500 disabled:border-gray-500 bg-transparent hover:bg-pink-500 text-pink-700 font-semibold hover:text-white py-2 px-4 border border-pink-500 hover:border-transparent rounded"
+          type="button"
+          onClick={resetAllButName}
+        >
+          Reset all but name
+        </button>
       </div>
-
-      <button
-        disabled={!isValid}
-        className="disabled:bg-gray-400 disabled:text-gray-500 disabled:border-gray-500 bg-transparent hover:bg-pink-500 text-pink-700 font-semibold hover:text-white py-2 px-4 border border-pink-500 hover:border-transparent rounded"
-        type="submit"
-      >
-        submit
-      </button>
-      <button
-        className="disabled:bg-gray-400 disabled:text-gray-500 disabled:border-gray-500 bg-transparent hover:bg-pink-500 text-pink-700 font-semibold hover:text-white py-2 px-4 border border-pink-500 hover:border-transparent rounded"
-        type="button"
-        onClick={randomize}
-      >
-        Rndom
-      </button>
     </form>
   );
 };
